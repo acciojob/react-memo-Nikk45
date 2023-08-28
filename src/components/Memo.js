622695a8da8c19c6f843f69d0a5fdde868a922ed
Nikk45 <nicksin36@gmail.com>
2023-08-28 15:20:51 +0530

    submit 1

---
+++ src/components/Memo.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+
+function Memo() {
+  const [todos, setTodos] = useState([]);
+  const [count, setCount] = useState(1);
+  const [inputText, setInputText] = useState('');
+  const [isTextValid, setIsTextValid] = useState(true);
+  const [amount, setAmount] = useState(1);
+    const [skills, setSkills] = useState([]);
+
+  const handleAddTodo = () => {
+    setTodos([...todos, 'New todo']);
+  };
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+    setAmount(count*90)
+  };
+
+  const handleInputChange = (event) => {
+    const newText = event.target.value;
+    setInputText(newText);
+    setIsTextValid(newText.length > 5);
+  };
+
+  const handleSubmit = () => {
+    if (isTextValid) {
+      setSkills([...skills, inputText]);
+      setInputText('');
+    }
+  };
+
+  useEffect(() => {
+    console.log('Todos updated:', todos);
+  }, [todos]);
+
+  return (
+    <div className="memo">
+      <div className='use-memo'>
+        <h1>React.useMemo</h1>
+        <h1>My todos</h1>
+        <div className="memo-list">
+        <p>New todos</p>
+        {todos.map((todo, index) => (
+          <p key={index}>{todo}</p>
+        ))}
+      </div>
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
+      <hr/>
+      <p>Count:{count} <button onClick={handleIncrement}>+</button></p>
+      <h1>Expensive Calculation</h1>
+      <p>{amount}</p>
+      <hr/>
+      <hr/>
+      <div className="memo-input">
+        <h1>React.memo</h1>
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="add skill"
+          className={isTextValid ? '' : 'invalid'}
+        />
+        <button onClick={handleSubmit}>
+          Add Skill
+        </button>
+      </div>
+
+      <ul className="memo-list">
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Memo;
